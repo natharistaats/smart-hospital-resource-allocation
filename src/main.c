61eae3c04ae161670e7d38aef277de1c9a05925e
natharistaats <natharistaats@gmail.com>
2026-08-29 21:43:11 +0530

    Displayed Doctor specialties

--- src/main.c
+++ src/main.c
@@ -31,6 +31,8 @@ const int dailyPatientCap[4] = {
     10
 };
 
+
+
 //Hospital ward lookup data
 const int wardID[4] = {1, 2, 3, 4};
 
@@ -59,9 +61,38 @@ const int bedCapacity[4] = {
 // 1 = Occupied
 int bedOccupancy[4][20] = {0}; //initially set all beds available
 
+
+//Display doctor specialties
+void displaySpecialties(void){
+    char row1[20] = "ID";
+    char row2[20] = "Specialty";
+    char row3[10] = "Fee";
+    char row4[25] = "Consultation Time(mins)";
+    char row5[20] = "Daily Patient Cap";
+
+    printf("\nDOCTOR SPECIALTIES\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("%-4s %-28s %-13s %-24s %s\n", row1, row2, row3, row4, row5);
+    printf("------------------------------------------------------------------------------------------\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%-4d %-28s Rs. %-15.2f %-24d %-20d\n",
+               specialtyID[i],
+               specialtyName[i],
+               baseFee[i],
+               consultationTime[i],
+               dailyPatientCap[i]);
+    }
+
+    printf("-------------------------------------------------------------------------------------------\n");
+}
+
+
 int main(void)
 {
     printf("Smart Hospital & Resource Allocation System\n");
+    displaySpecialties();
 
     return 0;
 }
